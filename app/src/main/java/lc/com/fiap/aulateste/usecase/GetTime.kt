89b33c767668fda660f15c395b5223cbd536b29f
caramelool <lucas.caramelo@mercadolivre.com>
2022-07-26 15:09:06 -0300

    formatted

--- app/src/main/java/lc/com/fiap/aulateste/usecase/GetTime.kt
+++ app/src/main/java/lc/com/fiap/aulateste/usecase/GetTime.kt
@@ -4,12 +4,12 @@ import java.text.SimpleDateFormat
 import java.util.Locale
 
 object GetTime {
-   private val timerFormat: SimpleDateFormat by lazy {
-       SimpleDateFormat("HH:mm:ss", Locale.getDefault())
-   }
+    private val timerFormat: SimpleDateFormat by lazy {
+        SimpleDateFormat("HH:mm:ss", Locale.getDefault())
+    }
 
-   operator fun invoke() : String {
-       val millis = System.currentTimeMillis()
-       return timerFormat.format(millis)
-   }
+    operator fun invoke(): String {
+        val millis = System.currentTimeMillis()
+        return timerFormat.format(millis)
+    }
 }
