4e4d06e94c5b603da8962047fb8e02ad4efe8415
caramelool <lucas.caramelo@mercadolivre.com>
2022-07-26 15:08:41 -0300

    simplify usecase

--- app/src/main/java/lc/com/fiap/aulateste/usecase/GetTime.kt
+++ app/src/main/java/lc/com/fiap/aulateste/usecase/GetTime.kt
@@ -2,18 +2,14 @@ package lc.com.fiap.aulateste.usecase
 
 import java.text.SimpleDateFormat
 import java.util.Locale
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
 
 object GetTime {
    private val timerFormat: SimpleDateFormat by lazy {
        SimpleDateFormat("HH:mm:ss", Locale.getDefault())
    }
 
-   suspend operator fun invoke() : String {
-       return withContext(Dispatchers.Default) {
-           val millis = System.currentTimeMillis()
-           timerFormat.format(millis)
-       }
+   operator fun invoke() : String {
+       val millis = System.currentTimeMillis()
+       return timerFormat.format(millis)
    }
 }
